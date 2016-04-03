9ae05a561a17554e8331a6b76423fc6b3b68aaeb
naoyuki <maeda@limu.ait.kyushu-u.ac.jp>
2016-04-03 19:15:47 +0900

    メイン画面を実装 (画面遷移はまだ)

--- app/src/main/java/app/naoyuki/piano/PianoActivity.java
+++ app/src/main/java/app/naoyuki/piano/PianoActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class PianoActivity extends AppCompatActivity {
 
@@ -17,6 +19,42 @@ public class PianoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_piano);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //ピアノ画面へ遷移
+        Button piano_btn=(Button)findViewById(R.id.go_to_piano);
+        piano_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(PianoActivity.this, "Click", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //音楽記号画面へ遷移
+        Button music_term_btn=(Button)findViewById(R.id.go_to_music_terms);
+        music_term_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //曲練習画面へ遷移
+        Button music_practice_btn=(Button)findViewById(R.id.go_to_music_practice);
+        music_practice_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        //リズム練習画面へ遷移
+        Button rhythm_practice_btn=(Button)findViewById(R.id.go_to_rhythm_practice);
+        rhythm_practice_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
